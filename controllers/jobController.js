49048df78a39ad751af150d51f219d92df2036af
LaibaAimen7777 <laibaimen7777@gmail.com>
2026-08-07 23:36:55 +0500

    8/7

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,5 +1,6 @@
 import { nanoid } from "nanoid";
 import Job from "../models/JobModel.js";
+import { StatusCodes } from "http-status-codes";
 
 let jobs = [
   { id: nanoid(), company: "apple", position: "front-end" },
@@ -8,7 +9,7 @@ let jobs = [
 
 export const getAllJobs = async (req, res) => {
   const jobs = await Job.find({});
-  res.status(200).json({ jobs });
+  res.status(StatusCodes.OK).json({ jobs });
 };
 
 export const getJob = async (req, res) => {
@@ -17,14 +18,14 @@ export const getJob = async (req, res) => {
   if (!job) {
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
-  res.status(200).json({ job });
+  res.status(StatusCodes.OK).json({ job });
 };
 
 export const createJob = async (req, res) => {
   const { company, position } = req.body;
   console.log(req.body);
   const job = await Job.create({ company, position });
-  res.status(200).json({ job });
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 export const updateJob = async (req, res) => {
@@ -34,7 +35,7 @@ export const updateJob = async (req, res) => {
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
 
-  res.status(200).json({ msg: "job modified", job: updatedJob });
+  res.status(StatusCodes.OK).json({ msg: "job modified", job: updatedJob });
 };
 
 export const deleteJob = async (req, res) => {
@@ -44,5 +45,5 @@ export const deleteJob = async (req, res) => {
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
 
-  res.status(200).json({ msg: "job deleted", job: deletedJob });
+  res.status(StatusCodes.OK).json({ msg: "job deleted", job: deletedJob });
 };
