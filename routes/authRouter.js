92485fb75dbadf34c24bff74a1fcbc09d6dc3f1f
LaibaAimen7777 <laibaimen7777@gmail.com>
2026-08-21 22:46:11 +0500

    8/21

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 const router = Router();
 import { login, register } from "../controllers/authController";
+import { validateRegisterInput } from "../middleware/validationMiddleware";
 
-router.post("./register", register);
+router.post("./register", validateRegisterInput, register);
 router.post("./login", login);
 
 export default router;
