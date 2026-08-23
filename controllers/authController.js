8175df18efb5751f78112ce14c089b973565465f
LaibaAimen7777 <laibaimen7777@gmail.com>
2026-08-23 23:51:00 +0500

    8/23

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,14 @@
 import User from "../models/UserModel";
 import { StatusCodes } from "http-status-codes";
+import bcrypt from "bcryptjs/dist/bcrypt";
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? "admin" : "user";
   const user = await User.create(req.body);
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  req.body.password = hashedPassword;
   res.status(StatusCodes.CREATED).json({ user });
 };
 export const login = async (req, res) => {};
