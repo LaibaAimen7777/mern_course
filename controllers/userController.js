2dfd4ba19984cfe3eb42e9a4fb1b9e348d491ccf
LaibaAimen7777 <laibaimen7777@gmail.com>
2026-09-08 23:13:40 +0500

    9/8

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,8 @@ import { StatusCodes } from "http-status-codes";
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId });
-  res.status(StatusCodes.OK).json({ user });
+  const userWithoutPassword = user.toJSON();
+  res.status(StatusCodes.OK).json({ user: userWithoutPassword });
 };
 
 export const getApplicationStats = async (req, res) => {
