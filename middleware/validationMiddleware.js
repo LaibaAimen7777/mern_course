fed4f7045ba35b415a1beb986803fe64dd6053ef
LaibaAimen7777 <laibaimen7777@gmail.com>
2026-09-09 13:36:50 +0500

    9/9

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -91,5 +91,16 @@ export const validateLoginInput = withValidationErrors([
 
 const validateUpdateUserInput = withValidationErrors([
   body("name").notEmpty().withMessage("name is required"),
-  body("email").notEmpty().withMessage("email is required"),
+  body("email")
+    .notEmpty()
+    .withMessage("email is required")
+    .isEmail()
+    .withMessage("Invalid email format")
+    .custom(async (email, { req }) => {
+      if (user && user._id.toString() !== req.user.userId) {
+        throw new Error("email already exists");
+      }
+    }),
+  body("lastName").notEmpty().withMessage("last name is required"),
+  body("location").notEmpty().withMessage("location is required"),
 ]);
